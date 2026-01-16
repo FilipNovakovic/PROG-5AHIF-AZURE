875d338c81bc577323958d01acbe057b4fb44798
FilipNovakovic <f.novakovic@students.htl-leonding.ac.at>
2026-01-16 13:58:34 +0100

    frontend fixed

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -2,15 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
+export interface Book {
+  id: number;
+  title: string;
+  price: number;
+}
+
+export interface CreateBookRequest {
+  title: string;
+  price: number;
+}
+
 @Injectable({ providedIn: 'root' })
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  hello() {
-    return this.http.get(`${environment.apiBaseUrl}/`, { responseType: 'text' });
+  getBooks() {
+    return this.http.get<Book[]>(`${environment.apiBaseUrl}/books`);
   }
 
-  getProducts() {
-    return this.http.get<any[]>(`${environment.apiBaseUrl}/products`);
+  addBook(req: CreateBookRequest) {
+    return this.http.post<Book>(`${environment.apiBaseUrl}/books`, req);
   }
 }
