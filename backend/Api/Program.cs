ec78b47e06fcc8f38d33b6a2ef0b4ca826db940a
DamjanTomas <d.tomas@students.htl-leonding.ac.at>
2026-01-16 12:58:24 +0100

    added smth for actions

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -10,17 +10,8 @@ builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-<<<<<<< HEAD
-var products = new List<Product>
-{
-    new(1, "Apple", 1.00m),
-    new(2, "Banana", 1.50m)
-};
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/products", () => products);
-=======
-
+        
 app.MapPost("/books", async (Book book, AppDbContext db) =>
 {
     if (book.Price <= 0)
@@ -33,9 +24,5 @@ app.MapPost("/books", async (Book book, AppDbContext db) =>
 
 app.MapGet("/books", async (AppDbContext db) =>
     await db.Books.ToListAsync());
->>>>>>> 48ba7c00906851ff8b637ee5c7ee68e490775f77
 
 app.Run();
-
-// Types / records immer ganz unten:
-record Product(int Id, string Name, decimal Price);
--- backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ backend/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cda2383302b6a251b6fabefc02591ec2938a5b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466b3775b5b9100e5f168ee8afc4cd91be05cd97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
