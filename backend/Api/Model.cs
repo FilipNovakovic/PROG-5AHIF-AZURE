ba44262a2e6b6d11212ce7d5bcb9de3acea3e025
DamjanTomas <d.tomas@students.htl-leonding.ac.at>
2026-01-16 13:03:23 +0100

    .

--- backend/Api/Model.cs
+++ backend/Api/Model.cs
@@ -5,7 +5,7 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; } = "";
     public decimal Price { get; set; }
-}
+} 
 
 
 public class AppDbContext : DbContext
